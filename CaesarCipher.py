bf8a6ef137d9f20035296bdd9c0ab408d16a06c3
MafiadeHacker <masekoedward010401@gmail.com>
2024-09-16 22:38:56 +0200

    Add Caesar Cipher function for text encryption

---
+++ CaesarCipher.py
@@ -0,0 +1,23 @@
+# Function to encrypt text using Caesar Cipher
+def encrypt(text, shift):
+    result = ""
+
+    # loop through each character in the text
+    for i in range(len(text)):
+        char = text[i]
+
+        # Encrypt uppercase characters
+        if char.isupper():
+            result += chr((ord(char) + shift - 65) % 26 + 65)
+
+            # Encrypt lowercase characters
+        elif char.islower():
+            result += chr((ord(char) + shift - 97) % 26 + 97)
+
+        # keep other characters unchanged
+        else:
+            result += char
+
+    return result
+
+# Function to decrypt text using Caesar Cipher
